d8e23240b1c247ee8504adaf1870b41657908fb8
Jonas Eukan <jonas.eukan@gmail.com>
2019-01-08 13:57:28 +0800

    Updated to use new SortAlgorithm class

--- Sort/Bubble.java
+++ Sort/Bubble.java
@@ -1,15 +1,13 @@
 package Sort;
 
-public class Bubble implements SortAlgorithm{
+public class Bubble extends SortAlgorithm {
 
     @Override
     public int[] sort(int[] array) {
         for(int i = array.length; i > 0; i--) {
             for(int j = 0; j < i - 1; j++) {
                 if(array[j] > array[j + 1]) {
-                    int temp = array[j];
-                    array[j] = array[j + 1];
-                    array[j + 1] = temp;
+                    array = super.swap(array, j, j + 1);
                 }
             }
         }
